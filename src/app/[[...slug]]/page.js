51b44fe89b895b230d9056a81c8a4693df0c7b3b
R S <r.stevenson@runbox.com>
2024-04-01 20:31:54 +1100

    adjust global css/js, configure ESLint

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -1,4 +1,4 @@
-import '../../index.css'
+import '../../css/index.css'
 import { ClientOnly } from './client'
  
 export function generateStaticParams() {
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
-import '../index.css';
+import Script from 'next/script'
+import '../css/normalize.css' // @csstools/Normalize CSS 12.1.1
+import '../css/bootstrap.min.css' // Bootstrap CSS 4.6.2
+import '../css/index.css'
 
 export const metadata = {
     title: 'React App',
@@ -10,18 +13,13 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <head>
             <meta name="theme-color" content="#000000" />
-
-            {/* <!-- @csstools/Normalize CSS --> */}
-            <link href="https://unpkg.com/@csstools/normalize.css" rel="stylesheet" />
-            {/* <!-- Bootstrap CSS --> */}
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossOrigin="anonymous" />
         </head>
         <body>
             <noscript>You need to enable JavaScript to run this app.</noscript>
             <div id="root">{children}</div>
 
-            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossOrigin="anonymous"></script>
+            <Script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossOrigin="anonymous" />
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" crossOrigin="anonymous" />
         </body>
         </html>
     )
